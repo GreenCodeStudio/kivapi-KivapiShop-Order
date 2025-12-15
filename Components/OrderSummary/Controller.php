fe7649042aca94dc78baa73d7256655a3b45a0b5
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-12-15 18:26:36 +0100

    .

---
+++ Components/OrderSummary/Controller.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace KivapiShop\Order\Components\OrderSummary;
+
+use Core\ComponentManager\ComponentController;
+use Core\Exceptions\NotFoundException;
+use KivapiShop\BasicProduct\Repository\ProductRepository;
+use KivapiShop\Order\Cart;
+
+class Controller extends ComponentController
+{
+
+
+    public string $cartId;
+    public object $deliveryDetails;
+    public array $cartItems;
+    public \Closure $formatCurrency;
+
+    public function __construct($params)
+    {
+        parent::__construct();
+        $this->formatCurrency = function ($amount) {
+            return number_format($amount / 100, 2, '.', '');
+        };
+        if (empty($_COOKIE['kshop_cartId'])) {
+            throw new NotFoundException();
+        } else {
+            $cart = new Cart($_COOKIE['kshop_cartId']);
+            $this->cartId = $_COOKIE['kshop_cartId'];
+            $this->deliveryDetails = $cart->getDeliveryDetails();
+            $this->cartItems = $cart->getItems();
+            if ($this->deliveryDetails->hasItems == 0) {
+                throw new NotFoundException();
+            }
+        }
+    }
+
+    public static function DefinedParameters()
+    {
+        return [
+            'cart_id' => (object)['type' => 'string', 'canFromQuery' => true],
+        ];
+    }
+
+    public function loadView()
+    {
+        $this->loadMPTS(__DIR__.'/View.mpts');
+    }
+
+}
